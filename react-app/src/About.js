e40e30688380ea53fa420577c4a2d6b176fd4486
Thierry Kozierow <thierry.kozierow@sanofi.com>
2021-05-12 14:13:24 +0200

    Modif

--- react-app/src/About.js
+++ react-app/src/About.js
@@ -3,11 +3,7 @@ import React from 'react';
 const About = () => (
   <div className="content-container">
     <div className="content-title-group not-found">
-<<<<<<< HEAD
       <h2 className="title"> **X** Product Wish List **X** </h2>
-=======
-      <h2 className="title">DEMO Product Wish List DEMO</h2>
->>>>>>> 558823f86102760c985dd320cc010f3036b5ca0c
       <p>
         This project was created to help represent a fundamental app written
         with React. The shopping theme is used throughout the app.
