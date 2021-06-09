a217e0ace6a6345b7ca5372dfaed87597993c1e7
Thierry Kozierow <thierry.kozierow@sanofi.com>
2021-06-09 16:03:37 +0200

    Grren Apple

--- react-app/src/store/product.api.js
+++ react-app/src/store/product.api.js
@@ -36,6 +36,12 @@ const data = {
       description: 'Bag of 12 big Tomatoes',
       quantity: 1,
     },
+    {
+      id: 50,
+      name: 'Tomatoes',
+      description: 'Bag of 12 big Green Apple',
+      quantity: 1,
+    },
   ],
 };
 
