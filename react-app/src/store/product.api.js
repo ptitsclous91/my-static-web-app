8f0e656bdd4911a884821b1c83cbe8da7caeffca
Thierry Kozierow <thierry.kozierow@sanofi.com>
2021-09-02 09:30:59 +0200

    450g package Strawberries

--- react-app/src/store/product.api.js
+++ react-app/src/store/product.api.js
@@ -3,7 +3,7 @@ const data = {
     {
       id: 10,
       name: 'Strawberries',
-      description: '16oz package of fresh organic strawberries',
+      description: '450g package of fresh organic strawberries',
       quantity: '1',
     },
     {
